9576a13eb79fa8f94ae5b5b962a7fa94045fbdb3
tamayozstore <manargad519@gmail.com>
2026-09-06 01:41:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TAMAYOZ_CONFIG = {
-  supabaseUrl: "https://mcqfkkgskpiwjcqzcuxr.supabase.co",
-  supabaseAnonKey: "sb_publishable_aiRU2MY_9yy-UYXUlnRA2Q_VeB7Ov4F",
+  supabaseUrl: "https://gspccsrvmcxngnyesouh.supabase.co",
+  supabaseAnonKey: "sb_publishable_cK-DJ_I5eZvuGRsuoHWgZg_5a2Udpx5",
   whatsappNumber: "201011703785"
 };
